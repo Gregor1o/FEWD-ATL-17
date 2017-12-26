4742fd1200ae796105a80d4d5fb3e08d7054202b
jose tejada <josetejada@MacBook-Pro.local>
2017-12-26 09:33:33 -0500

    h

--- Week 4/lesson 08 - Intro to jQuery/starter_code/traffic_light/js/index.js
+++ Week 4/lesson 08 - Intro to jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,15 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+$(document).ready(function() {
+	// alert("Hello World");
+
+$('#stopButton').click(illuminateRed);
+$('#slowButton').click(illuminateYellow);
+$('#goButton#').click(illuminateGreen);
+
+	function illuminateRed() {
+		clearlights();
+		$("#stopLight").css('background',  'red');
+}
+
+}};
